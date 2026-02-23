5c35732abc258215015062c01e0404d21b5fcc59
johannescairns <43068245+johannescairns@users.noreply.github.com>
2026-02-23 13:35:50 +0000

    Deploying to gh-pages from @ johannescairns/meg-lab@300f98c05c16d1592e4876236d7721a0e5ab991d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,8 +391,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/meg-lab/books/the_godfather/";
-            },},{id: "news-pi-johannes-cairns-was-appointed-collegium-researcher-at-the-turku-collegium-for-science-medicine-and-technology",
-          title: 'PI Johannes Cairns was appointed Collegium Researcher at the Turku Collegium for Science,...',
+            },},{id: "news-johannes-was-appointed-collegium-researcher-at-the-turku-collegium-for-science-medicine-and-technology",
+          title: 'Johannes was appointed Collegium Researcher at the Turku Collegium for Science, Medicine and...',
           description: "",
           section: "News",},{id: "news-pi-johannes-cairns-was-appointed-assistant-professor-ddls-fellow-at-the-faculty-of-medicine-lund-university-and-scilifelab-here-is-his-interview-on-the-appointment-at-lund",
           title: 'PI Johannes Cairns was appointed Assistant Professor (DDLS Fellow) at the Faculty of...',
