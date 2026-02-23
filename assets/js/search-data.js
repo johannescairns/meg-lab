1d3a89114e2625487f845fc9a998c04f2649356d
johannescairns <43068245+johannescairns@users.noreply.github.com>
2026-02-23 09:47:50 +0000

    Deploying to gh-pages from @ johannescairns/meg-lab@3dd3204aeb070278ed9e9865a6ef9855fa69baf6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/meg-lab/";
     },
-  },{id: "nav-about",
-          title: "about",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/meg-lab/";
-          },
-        },{id: "nav-people",
+  },{id: "nav-people",
           title: "people",
           description: "members of the lab",
           section: "Navigation",
