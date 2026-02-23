047e057226f94c8fa396a4863437e5b4b84b3243
johannescairns <43068245+johannescairns@users.noreply.github.com>
2026-02-23 08:49:19 +0000

    Deploying to gh-pages from @ johannescairns/meg-lab@c84cdd07f97cb6049c21d967cb91900a0c8523a8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,21 +51,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/meg-lab/people/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/meg-lab/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/meg-lab/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
